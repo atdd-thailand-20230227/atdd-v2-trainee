2c206c7c625769d8fe34eafb08562013d8d8187a
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2023-01-22 10:53:06 +0800

    :white_check_mark: cherry pick from previous step

--- src/test/java/com/odde/atddv2/LoginSteps.java
+++ src/test/java/com/odde/atddv2/LoginSteps.java
@@ -3,9 +3,9 @@ package com.odde.atddv2;
 import com.odde.atddv2.entity.User;
 import com.odde.atddv2.page.HomePage;
 import com.odde.atddv2.repo.UserRepo;
-import io.cucumber.java.zh_cn.假如;
-import io.cucumber.java.zh_cn.当;
-import io.cucumber.java.zh_cn.那么;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class LoginSteps {
@@ -18,24 +18,24 @@ public class LoginSteps {
     @Autowired
     private UserRepo userRepo;
 
-    @假如("存在用户名为{string}和密码为{string}的用户")
-    public void 存在用户名为和密码为的用户(String userName, String password) {
+    @Given("exists a user with username {string} and password {string}")
+    public void existsAUserWithUsernameAndPassword(String userName, String password) {
         userRepo.save(new User().setUserName(userName).setPassword(password));
     }
 
-    @当("以用户名为{string}和密码为{string}登录时")
-    public void 以用户名为和密码为登录时(String userName, String password) {
+    @When("I login with username {string} and password {string}")
+    public void iLoginWithUsernameAndPassword(String userName, String password) {
         homePage.open();
         homePage.login(userName, password);
     }
 
-    @那么("{string}登录成功")
-    public void 登录成功(String userName) {
+    @Then("{string} should be logged in")
+    public void shouldBeLoggedIn(String userName) {
         browser.shouldHaveText("Welcome " + userName);
     }
 
-    @那么("登录失败的错误信息是{string}")
-    public void 登录失败的错误信息是(String message) {
+    @Then("login failed error message should be {string}")
+    public void loginFailedErrorMessageShouldBe(String message) {
         browser.shouldHaveText(message);
     }
 }
