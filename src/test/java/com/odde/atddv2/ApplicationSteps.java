19cb28423be6a198be74e072581b08ab93e1bd1d
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2023-01-23 21:32:08 +0800

    :white_check_mark: cherry pick from previous step

--- src/test/java/com/odde/atddv2/ApplicationSteps.java
+++ src/test/java/com/odde/atddv2/ApplicationSteps.java
@@ -3,6 +3,7 @@ package com.odde.atddv2;
 import com.github.leeonky.cucumber.restful.RestfulStep;
 import com.odde.atddv2.repo.OrderRepo;
 import com.odde.atddv2.repo.UserRepo;
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,17 +22,24 @@ public class ApplicationSteps {
     @Autowired
     private OrderRepo orderRepo;
 
+    @Autowired
+    private Browser browser;
+    @Autowired
+    private RestfulStep restfulStep;
+
     @Before(order = 1)
     public void clearDB() {
         userRepo.deleteAll();
         orderRepo.deleteAll();
     }
 
-    @Autowired
-    private RestfulStep restfulStep;
-
     @PostConstruct
     public void setBaseUrl() {
         restfulStep.setBaseUrl("http://127.0.0.1:10081/api/");
+    }
+
+    @After
+    public void closeBrowser() {
+        browser.quit();
     }
 }
