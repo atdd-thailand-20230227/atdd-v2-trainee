5c6f3543bba3d9dc115d0fb95cc637330036f1e8
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2023-02-11 18:09:43 +0800

    :sparkles: update translation

--- src/test/java/com/odde/atddv2/TestSteps.java
+++ src/test/java/com/odde/atddv2/TestSteps.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.odde.atddv2.entity.User;
 import com.odde.atddv2.repo.UserRepo;
 import io.cucumber.java.After;
-import io.cucumber.java.zh_cn.假如;
-import io.cucumber.java.zh_cn.当;
-import io.cucumber.java.zh_cn.那么;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import lombok.SneakyThrows;
 import okhttp3.*;
 import org.openqa.selenium.By;
@@ -32,10 +32,47 @@ public class TestSteps {
         return new RemoteWebDriver(new URL("http://web-driver.tool.net:4444"), DesiredCapabilities.chrome());
     }
 
-    @当("测试环境")
-    public void 测试环境() {
+    @Given("exists a user with username {string} and password {string}")
+    public void existsAUserWithUsernameAndPassword(String userName, String password) {
+        userRepo.deleteAll();
+        userRepo.save(new User().setUserName(userName).setPassword(password));
+    }
+
+    @SneakyThrows
+    @When("api login with username {string} and password {string}")
+    public void apiLoginWithUsernameAndPassword(String userName, String password) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        ObjectMapper objectMapper = new ObjectMapper();
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), objectMapper.writeValueAsString(new User().setUserName(userName).setPassword(password)));
+        Request request = new Request.Builder().url("http://localhost:10081/users/login").post(requestBody).build();
+        response = okHttpClient.newCall(request).execute();
+    }
+
+    @Then("print token")
+    public void printToken() {
+        System.out.println("response.header(\"token\") = " + response.header("token"));
+    }
+
+    @When("search {string} with baidu")
+    public void searchWithBaidu(String keyword) {
+        getWebDriver().get("http://www.baidu.com");
+        webDriver.findElement(By.xpath("//*[@id='kw']")).sendKeys(keyword);
+        webDriver.findElement(By.xpath("//*[@id='su']")).click();
+    }
+
+    @SneakyThrows
+    @Then("print search result count")
+    public void printSearchResultCount() {
+        TimeUnit.SECONDS.sleep(2);
+        String text = webDriver.findElement(xpath("//*[@id='container']/div[2]/div/div[2]/span")).getText();
+        System.out.println("text = " + text);
+    }
+
+    @Then("test environment should be ok")
+    public void testEnvironmentShouldBeOk() {
         getWebDriver().get("http://host.docker.internal:10081/");
-        assertThat(getWebDriver().findElements(xpath("//*[text()='登录']"))).isNotEmpty();
+        assertThat(getWebDriver().findElements(xpath("//*[text()='Login']"))).isNotEmpty();
+        getWebDriver().quit();
     }
 
     @After
@@ -44,43 +81,6 @@ public class TestSteps {
             webDriver.quit();
             webDriver = null;
         }
-    }
-
-    @那么("打印Token")
-    public void 打印_token() {
-        System.out.println("response.header(\"token\") = " + response.header("token"));
-    }
-
-    @SneakyThrows
-    @那么("打印百度为您找到的相关结果数")
-    public void 打印百度为您找到的相关结果数() {
-        TimeUnit.SECONDS.sleep(2);
-        String text = webDriver.findElement(xpath("//*[@id='container']/div[2]/div/div[2]/span")).getText();
-        System.out.println("text = " + text);
-    }
-
-    @假如("存在用户名为{string}和密码为{string}的用户")
-    public void 存在用户名为和密码为的用户(String userName, String password) {
-        userRepo.deleteAll();
-        userRepo.save(new User().setUserName(userName).setPassword(password));
-    }
-
-    @SneakyThrows
-    @当("通过API以用户名为{string}和密码为{string}登录时")
-    public void 通过api以用户名为和密码为登录时(String userName, String password) {
-        OkHttpClient okHttpClient = new OkHttpClient();
-        ObjectMapper objectMapper = new ObjectMapper();
-        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), objectMapper.writeValueAsString(new User().setUserName(userName).setPassword(password)));
-        Request request = new Request.Builder().url("http://localhost:10081/users/login").post(requestBody).build();
-        response = okHttpClient.newCall(request).execute();
-    }
-
-    @SneakyThrows
-    @当("在百度搜索关键字{string}")
-    public void 在百度搜索关键字(String keyword) {
-        getWebDriver().get("http://www.baidu.com");
-        webDriver.findElement(By.xpath("//*[@id='kw']")).sendKeys(keyword);
-        webDriver.findElement(By.xpath("//*[@id='su']")).click();
     }
 
     public WebDriver getWebDriver() {
